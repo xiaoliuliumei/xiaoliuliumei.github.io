2c1a2a33d35840907a3fa1d2594aec4fe3acc4ba
xiaoliuliumei <101188623+xiaoliuliumei@users.noreply.github.com>
2022-03-11 12:23:25 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     // 你的所有代码都应该写在这里
     let targetImage = document.querySelector("#smart-image");
-    console.log("1")
+    console.log(targetImage)
       targetImage.addEventListener("click", function() {
     alert("点击了图片"); 
 });
